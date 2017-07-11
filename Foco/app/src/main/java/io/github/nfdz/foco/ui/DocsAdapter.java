284d1333ed2d1418b358b295070c6258189ad758
nfdz <noe.fernandez@protonmail.com>
2017-07-11 22:22:56 +0200

    implement main ui

--- Foco/app/src/main/java/io/github/nfdz/foco/ui/DocsAdapter.java
+++ Foco/app/src/main/java/io/github/nfdz/foco/ui/DocsAdapter.java
@@ -7,7 +7,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.github.nfdz.foco.R;
 
 public class DocsAdapter extends RecyclerView.Adapter<DocsAdapter.ViewHolder> {
@@ -36,7 +39,9 @@ public class DocsAdapter extends RecyclerView.Adapter<DocsAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(DocsAdapter.ViewHolder holder, int position) {
-
+        holder.title.setText("Title etc");
+        holder.words.setText("1500");
+        holder.time.setText("40 min");
     }
 
     @Override
@@ -44,17 +49,29 @@ public class DocsAdapter extends RecyclerView.Adapter<DocsAdapter.ViewHolder> {
         return 5;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class ViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener, View.OnLongClickListener {
+
+        @BindView(R.id.doc_item_title) TextView title;
+        @BindView(R.id.doc_item_time) TextView time;
+        @BindView(R.id.doc_item_words) TextView words;
 
         public ViewHolder(View itemView) {
             super(itemView);
-
+            ButterKnife.bind(this, itemView);
             itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
+            if (mHandler != null) mHandler.onDocumentClick();
+        }
 
+        @Override
+        public boolean onLongClick(View v) {
+            if (mHandler != null) mHandler.onDocumentLongClick();
+            return true;
         }
     }
 }
